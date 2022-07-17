16f32ad5b902fac6f2eab78f055a23df38589653
Jenish Patel <jenish@ualberta.ca>
2022-07-17 13:28:25 -0600

    Updated entrypoint.sh #16

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Custom for ex2_obc_software repo
-./configure_repo.sh
-
 # Import project
 eclipse -noSplash -data ~/ccs-workspace -application com.ti.ccstudio.apps.projectImport -ccs.location /github/workspace/
 # compile all imported projects
