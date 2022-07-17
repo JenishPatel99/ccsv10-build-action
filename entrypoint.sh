91017f3fc08ebb5e4d186692aaa84fe86392361d
Jenish Patel <jenish@ualberta.ca>
2022-07-17 09:43:48 -0600

    Updated entrypoint.sh #13

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,10 @@ echo "List ex2_obc_software"
 ls /home/runner/ex2_obc_software/ex2_obc_software/
 
 echo "List ex2_obc_software #2"
+ls /github/
 ls /github/workspace/ex2_obc_software
+ls ~/github/
+ls ~/github/workspace/ex2_obc_software
 
 # Add repo (project) to ccs-workspace
 eclipse -noSplash -data ~/ccs-workspace -application com.ti.ccstudio.apps.projectImport -ccs.location /home/runner/work/ex2_obc_software/ex2_obc_software
@@ -39,6 +42,8 @@ ls -a
 cd /root/
 echo "List root dir"
 ls -a
+
+ls ~/github/workspace/ex2_obc_software
 
 # compile all imported projects
 eclipse -noSplash -data ~/ccs-workspace -application com.ti.ccstudio.apps.projectBuild -ccs.workspace -ccs.autoImport -ccs.listErrors
