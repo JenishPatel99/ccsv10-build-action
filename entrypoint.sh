78210d29f738f17fb4ff8389304af8a6ea03a640
Jenish Patel <jenish@ualberta.ca>
2022-07-16 22:15:21 -0600

    Action ready and to be tested

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+# Add repo (project) to ccs-workspace
+eclipse -noSplash -data ~/ccs-workspace -application com.ti.ccstudio.apps.projectImport -ccs.location ${{ github.event.repository.name }}
+# compile all imported projects
+eclipse -noSplash -data ~/ccs-workspace -application com.ti.ccstudio.apps.projectBuild -ccs.workspace -ccs.autoImport -ccs.listErrors
+
+retcode=echo $?
+
+if [ retcode = 0 ]
+then
+	exit 0
+else
+	exit 1
+fi
